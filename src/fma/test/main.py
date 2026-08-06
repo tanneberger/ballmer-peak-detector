48403f9a842086789ddec8027dfec4d2890dedf6
tanneberger <github@tanneberger.me>
2026-08-06 21:18:48 +0200

    read data from csw

--- src/fma/test/main.py
+++ src/fma/test/main.py
@@ -1,38 +1,30 @@
 import cocotb
 from cocotb.triggers import FallingEdge, Timer
-import random
-import softposit as sp
-from softposit import posit8
+#import random
+#import softposit as sp
+#from softposit import posit8
 
 @cocotb.test()
 async def random_posit_test(dut):
     
-    for i in range(10):
-        dut.clk.value = 0
+    with open("testdata.csv", "rw") as f:
+        for line in f.readlines():
+            line_split = line.split(" ")
+            dut.clk.value = 0
 
-        a = random.random() * 10
-        b = random.random() * 10
-        c = random.random() * 10
-        
-        p_2_a = posit8(a)
-        p_2_b = posit8(b)
-        p_2_c = posit8(c)
-
-        dut.i_opa.value = int(p_2_a)
-        dut.i_opb.value = int(p_2_b)
-        dut.i_opc.value = int(p_2_c)
-        dut.clk.value = 1
-        await Timer(200, unit="ns")  # wait a bit
-        
-        expected_value = p_2_c.fma(p_2_a, p_2_b)
-        received_value = posit8(bits=dut.o_result.value.integer)
-        cocotb.log.info("%f * %f + %f = %s = %s", a, b, c, str(expected_value), str(received_value))
-        p_2_a.toBinary()
-        p_2_b.toBinary()
-        p_2_c.toBinary()
-        expected_value.toBinary()
-        received_value.toBinary()
-        
+            a = int(line_split[0], 16)
+            b = int(line_split[1], 16)
+            c = int(line_split[2], 16)
+            expected = int(line_split[3], 16)
+            
+            dut.i_opa.value = a
+            dut.i_opb.value = b
+            dut.i_opc.value = c
+            dut.clk.value = 1
+            await Timer(200, unit="ns")  # wait a bit
+            
+            cocotb.log.info("%i * %i + %i = %i = %i", a, b, c, expected, dut.o_result.value.integer)
+       
     #await FallingEdge(dut.clk)  # wait for falling edge/"negedge"
 
     #assert dut.my_signal_2.value == 0
